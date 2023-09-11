97b3032be0b8ee68f9a70ca1d748c5b18b84e7f9
rian <rianb@CENTRAL-DESK.(none)>
2023-09-11 12:48:56 -0300

    first commit

---
+++ first-challenge/src/main/java/com/challenge/firstchallenge/FirstChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.firstchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstChallengeApplication.class, args);
+	}
+
+}
---
+++ first-challenge/src/test/java/com/challenge/firstchallenge/FirstChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.firstchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
